36e49c5f8910664b9b2802f1f9e261c276c3bc0c
Louis Aurick <louisaurick@ntymail.com>
2018-06-27 12:38:39 +0200

    start of a readme

--- index.js
+++ index.js
@@ -1,42 +1,24 @@
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+module.exports = Phrase;
+
+
+String.prototype.reverse = function() {
+	return Array.from(this).reverse().join("");
 }
 
 // Defines a Phrase object.
 function Phrase(content) {
 	this.content = content;
 	// Returns content processed for palindrome testing.
-	this.processor = function(string) {
-		return string.toLowerCase();
-	}
+
 	this.processedContent = function() {
-		return this.processor(this.content);
+		return this.content.toLowerCase();
 	}
 	// Returns true for a palindrome, false otherwise.
 	this.palindrome = function() {
-		return this.processedContent() === reverse(this.processedContent());
-	}
-	this.louder = function () {
-		return louderContent = this.content.toUpperCase();
+    return this.processedContent() === this.processedContent().reverse();
 	}
 }
 
-function TranslatedPhrase(content,translation) {
-  this.content = content;
-  this.translation = translation;
-  // Returns translation processed for palindrome testing.
-  this.processedContent = function processedContent() {
-  	return this.processor(this.translation);
-  }
-}
-
-
-phrase = new Phrase("Racecar");
-console.log(phrase.palindrome());
-console.log(phrase.louder());
-
-
-TranslatedPhrase.prototype = new Phrase();
-let frase = new TranslatedPhrase("prout", "lolilol");
-console.log(frase.palindrome());
+let napoleonsLament = new Phrase("aurevoir");
+console.log(napoleonsLament.palindrome());
+console.log(napoleonsLament.isBlank());
---
+++ readme.md
@@ -0,0 +1,5 @@
+#Phrase object with palindrome detector
+
+this is a sample NPM module created in [*Lern Enough Javascirpt to Be Dangerous*](https://www.learnenough.com/javascript-tutorial) by Michael Hartl.
+
+The module can be used as follows:
